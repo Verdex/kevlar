42776f6d265e1d02c2a28e306862dcd071aacf81
David L. Inman <davidlevere@gmail.com>
2019-09-11 23:22:15 -0400

    file with some examples; main file with main functions that need to be registered

---
+++ main.lua
@@ -0,0 +1,44 @@
+
+local example = require 'example'
+
+-- this only gets called once at the beginning
+function love.load()
+
+end
+
+-- this function is called continuously
+-- dt is the delta time (in seconds?) of the last
+-- time that the function was called
+function love.update(dt)
+
+end
+
+-- this is the only function that the graphics functions
+-- will work in
+function love.draw()
+    example.x()
+end
+
+function love.mousepressed(x, y, button, istouch)
+    
+end
+
+function love.mousereleased(x, y, button, istouch)
+
+end
+
+function love.keypressed(key)
+
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.focus(in_focus)
+
+end
+
+function love.quit()
+
+end
