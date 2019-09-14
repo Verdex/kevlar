046ede8da7a40fd64f61f21831a9bddbffe811fc
David L. Inman <davidlevere@gmail.com>
2019-09-13 22:25:17 -0400

    added support to forward keys to different listeners

--- keyboard_listener.lua
+++ keyboard_listener.lua
@@ -7,7 +7,15 @@ local keyShiftDict = { ['1'] = '!'
 local function init()
     return { left_shift = false
            ; right_shift = false
+           ; mode = false
+           ; modes = {} 
            }
+end
+
+local function set_mode( listener, mode_letter, forwardee )
+    listener.modes[#listener.modes + 1] = { mode = mode_letter
+                                          ; forwardee = forwardee 
+                                          }
 end
 
 local function shift_key( key )
@@ -22,19 +30,34 @@ local function keypress( listener, key )
     end
 
     if key == "escape" then
-
+        listener.mode = false
     elseif key == "lshift" then 
         listener.left_shift = true
     elseif key == "rshift" then
         listener.right_shift = true
+    elseif not listener.mode then
+        for _, v in ipairs( listener.modes ) do
+            if v.mode == key then
+                listener.mode = v.forwardee
+                break
+            end
+        end
+    else
+        listener.mode:keypress( key )
     end
 end
 
 local function keyrelease( listener, key )
+    if listener.left_shift or listener.right_shift then
+        key = shift_key( key )
+    end
+
     if key == "lshift" then 
         listener.left_shift = false
     elseif key == "rshift" then
         listener.right_shift = false 
+    elseif listener.mode then
+        listener.mode:keyrelease( key )
     end
 end
 
