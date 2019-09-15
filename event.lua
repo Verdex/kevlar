6b2b94d945b71e23b2e56e1317267c8d2a74c487
David L. Inman <davidlevere@gmail.com>
2019-09-14 21:43:51 -0400

    event processor

--- event.lua
+++ event.lua
@@ -1,13 +1,22 @@
 
-local function init()
-    local e = {}
+local function init( logger )
+    local e = { listeners = {} }
 
-    function e:register_listener()
-
+    function e:register_listener( event, register, callback )
+        logger:info( string.format( "%s registered a callback for %s event", register, event ) )
+        if not self.listeners[event] then
+            self.listeners[event] = {}
+        end
+        local event_list = self.listeners[event]
+        event_list[#event_list+1] = { callback = callback ; register = register }
     end
 
     function e:trigger( event, options )
-
+        local registered_listeners = self.listeners[event]
+        if not registered_listeners then return end
+        for _, v in ipairs(registered_listeners) do
+            v.callback( options )
+        end
     end
 
     return e
